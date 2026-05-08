7609cede269501f8d3f341bae209cae574554a5c
Joffrey Dumont Le Brazidec <joffrey.dumont@ecmwf.int>
2026-05-08 14:46:16 +0000

    feat(eval): export prepml modules from eval.predict

--- eval/predict/__init__.py
+++ eval/predict/__init__.py
@@ -9,8 +9,17 @@ from .dataset_builder import build_prediction_dataset, validate_predictions_data
 from .distributed_io import Rank0FileWriter, _destroy_process_group, _distributed_barrier
 from .inference_engine import predict_ensemble_members, predict_single_bundle
 from .main import create_parser, main
+from .mars_retrieve import (
+    assemble_predictions_file,
+    build_prediction_request,
+    group_weather_states_for_mars,
+    retrieve_predictions_from_mars,
+    weather_state_to_mars,
+)
 from .model_loader import load_inference_model, setup_distributed
 from .output_writer import prediction_output_path, write_predictions_file
+from .prepml import prepml_predict, resolve_expver
+from .prepml_config import generate_prepml_config, write_prepml_config
 from .types import BundleKey, EnsemblePrediction, PredictionConfig, PredictionMetadata, PredictionResult
 
 __all__ = [
@@ -23,18 +32,27 @@ __all__ = [
     "Rank0FileWriter",
     "_destroy_process_group",
     "_distributed_barrier",
+    "assemble_predictions_file",
     "build_prediction_dataset",
+    "build_prediction_request",
     "create_parser",
     "date_str_to_datetime64",
     "discover_bundles",
+    "generate_prepml_config",
+    "group_weather_states_for_mars",
     "load_inference_model",
     "main",
     "parse_bundle_key",
     "predict_ensemble_members",
     "predict_single_bundle",
     "prediction_output_path",
+    "prepml_predict",
     "resolve_date_step_pairs",
+    "resolve_expver",
+    "retrieve_predictions_from_mars",
     "setup_distributed",
     "validate_predictions_dataset",
+    "weather_state_to_mars",
+    "write_prepml_config",
     "write_predictions_file",
 ]
